fc77929af4e464f37213ebe1823765007d5c9ede
ke he <smileshitou@hotmail.com>
2016-02-04 01:14:34 +0800

    Initial Commit

---
+++ hxDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  hxDemo
+//
+//  Created by heke on 16/2/4.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ hxDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  hxDemo
+//
+//  Created by heke on 16/2/4.
+//  Copyright © 2016年 mhk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
